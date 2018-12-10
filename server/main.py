49f71066a82f06e9eaa862fd9886f57964179c4a
Heye Everts <heye.everts.1@gmail.com>
2018-12-10 13:17:51 +0100

    update

--- server/main.py
+++ server/main.py
@@ -55,4 +55,4 @@ if __name__ == '__main__':
         keyfile=keyPath
     )    
     #app.run(host=hostAddr, port=4431, ssl=ctx, workers=4)
-    app.run(host=hostAddr, port=8080, workers=4)
+    app.run(host=hostAddr, port=8080, workers=1)
